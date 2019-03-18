3ce72932d61e5dea95d92e917cd254a7a5075340
almajd fares <almajd.fares@gmail.com>
2019-03-18 10:25:49 +0100

    admin_panel_exercise.sql

--- admin_panel_exercise.sql
+++ admin_panel_exercise.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb5
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Mar 18, 2019 at 10:08 AM
--- Server version: 5.7.25-0ubuntu0.18.04.2
--- PHP Version: 7.2.15-0ubuntu0.18.04.1
+-- Host: 127.0.0.1
+-- Generation Time: Mar 18, 2019 at 10:24 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,10 +25,46 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `User`
+-- Table structure for table `items`
 --
 
-CREATE TABLE `User` (
+CREATE TABLE `items` (
+  `item_ID` int(11) NOT NULL,
+  `itemName` varchar(56) NOT NULL,
+  `itemDate` date NOT NULL,
+  `itemStatus` text NOT NULL,
+  `itemType` varchar(34) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status`
+--
+
+CREATE TABLE `status` (
+  `status_ID` int(11) NOT NULL,
+  `statusName` varchar(43) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `type`
+--
+
+CREATE TABLE `type` (
+  `type_ID` int(11) NOT NULL,
+  `typeName` varchar(43) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
   `User_ID` int(11) NOT NULL,
   `userName` varchar(30) NOT NULL,
   `email` varchar(60) NOT NULL,
@@ -34,10 +72,10 @@ CREATE TABLE `User` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `User`
+-- Dumping data for table `user`
 --
 
-INSERT INTO `User` (`User_ID`, `userName`, `email`, `userPassword`) VALUES
+INSERT INTO `user` (`User_ID`, `userName`, `email`, `userPassword`) VALUES
 (1, 'Simon', 'simon@domain.com', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92');
 
 --
@@ -45,9 +83,27 @@ INSERT INTO `User` (`User_ID`, `userName`, `email`, `userPassword`) VALUES
 --
 
 --
--- Indexes for table `User`
+-- Indexes for table `items`
 --
-ALTER TABLE `User`
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`item_ID`);
+
+--
+-- Indexes for table `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`status_ID`);
+
+--
+-- Indexes for table `type`
+--
+ALTER TABLE `type`
+  ADD PRIMARY KEY (`type_ID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
   ADD PRIMARY KEY (`User_ID`);
 
 --
@@ -55,10 +111,30 @@ ALTER TABLE `User`
 --
 
 --
--- AUTO_INCREMENT for table `User`
+-- AUTO_INCREMENT for table `items`
 --
-ALTER TABLE `User`
+ALTER TABLE `items`
+  MODIFY `item_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `status`
+--
+ALTER TABLE `status`
+  MODIFY `status_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `type`
+--
+ALTER TABLE `type`
+  MODIFY `type_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
   MODIFY `User_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
